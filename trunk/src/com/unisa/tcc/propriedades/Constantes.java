aa0f3bcdf7cc8c0dea5a6aec86283ead3915ec47
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-08-15 04:03:40 +0000

    Alterando o nome do projeto e copiando os arquivos do projeto antigo

---
+++ trunk/src/com/unisa/tcc/propriedades/Constantes.java
@@ -0,0 +1,9 @@
+package com.unisa.tcc.propriedades;
+
+public class Constantes {
+
+	public static final String TIPO_PROFESSOR = "professor";
+	public static final String TIPO_ALUNO = "aluno";
+	public static final String TIPO_ADMINISTRADOR = "administrador";
+	public static final String OK = "OK";
+}
