b742578d3c4d8a0d22a52b85bece04dceaeb142f
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-09-06 04:20:07 +0000

    Alteração da entidade de TO

---
+++ trunk/dce/src/com/unisa/tcc/to/ProfessorTo.java
@@ -0,0 +1,34 @@
+package com.unisa.tcc.to;
+
+public class ProfessorTo {
+
+	private String nome;
+	private int idProfessor;
+	private String usuario;
+	private String senha;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdProfessor() {
+		return idProfessor;
+	}
+	public void setIdProfessor(int idProfessor) {
+		this.idProfessor = idProfessor;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
