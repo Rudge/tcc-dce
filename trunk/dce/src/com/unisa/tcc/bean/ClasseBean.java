d3b014436a6a65c2a3d16251c47dbe09c459534e
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-10-03 19:28:07 +0000

    Alteração da entidade classe.

--- trunk/dce/src/com/unisa/tcc/bean/ClasseBean.java
+++ trunk/dce/src/com/unisa/tcc/bean/ClasseBean.java
@@ -1,5 +1,34 @@
 package com.unisa.tcc.bean;
 
 public class ClasseBean {
-
+	
+	private String serie;
+	private String periodo;
+	private ChamadaBean chamada;
+	private int ano;
+	
+	public String getSerie() {
+		return serie;
+	}
+	public void setSerie(String serie) {
+		this.serie = serie;
+	}
+	public String getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(String periodo) {
+		this.periodo = periodo;
+	}
+	public ChamadaBean getChamada() {
+		return chamada;
+	}
+	public void setChamada(ChamadaBean chamada) {
+		this.chamada = chamada;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
 }
