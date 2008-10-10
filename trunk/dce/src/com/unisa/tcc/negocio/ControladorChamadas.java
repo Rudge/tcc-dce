bf65357bca75aa80573a0e2683b7a29c7aa6e07c
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-10-10 21:07:36 +0000

    Criação da classe de excessão

--- trunk/dce/src/com/unisa/tcc/negocio/ControladorChamadas.java
+++ trunk/dce/src/com/unisa/tcc/negocio/ControladorChamadas.java
@@ -4,10 +4,11 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.unisa.tcc.dao.ControladorChamadasDAO;
+import com.unisa.tcc.propriedades.DceException;
 
 public class ControladorChamadas {
 	
-	public List<?> consultarChamadas(int idProfessor, GregorianCalendar data){
+	public List<?> consultarChamadas(int idProfessor, GregorianCalendar data) throws DceException{
 		ControladorChamadasDAO controladorDAO = new ControladorChamadasDAO();
 		
 		
