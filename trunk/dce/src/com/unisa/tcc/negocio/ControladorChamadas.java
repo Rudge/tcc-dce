0015d6cc26c2542e2ac1c556fddba10b133acf34
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-10-27 19:47:32 +0000

    Criação da consulta de chamada por data.

--- trunk/dce/src/com/unisa/tcc/negocio/ControladorChamadas.java
+++ trunk/dce/src/com/unisa/tcc/negocio/ControladorChamadas.java
@@ -1,20 +1,40 @@
 package com.unisa.tcc.negocio;
 
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import com.unisa.tcc.bean.ChamadaBean;
 import com.unisa.tcc.dao.ControladorChamadasDAO;
 import com.unisa.tcc.propriedades.DceException;
+import com.unisa.tcc.to.ChamadaTo;
+import com.unisa.tcc.to.ClasseTo;
+import com.unisa.tcc.to.DisciplinaTo;
 
 public class ControladorChamadas {
 	
-	public List<?> consultarChamadas(int idProfessor, GregorianCalendar data) throws DceException{
+	public List<ChamadaTo> consultarChamadasPorData(int idProfessor, String data) throws DceException{
 		ControladorChamadasDAO controladorDAO = new ControladorChamadasDAO();
+		List<ChamadaTo> listaChamadasTo = new ArrayList<ChamadaTo>(); 
+		String[] arrData = data.split("/");
+		int ano = Integer.parseInt(arrData[2]);
+		int mes = Integer.parseInt(arrData[1]);
+		int dia = Integer.parseInt(arrData[0]);
+		List<ChamadaBean> listaChamadasBean = controladorDAO.consultarChamadasPorData(idProfessor, new GregorianCalendar(ano,mes,dia));
 		
-		
-		controladorDAO.consultarChamada(idProfessor, data);
-		
-		return null;
+		for (ChamadaBean chamadaBean : listaChamadasBean) {
+			ChamadaTo chamadaTo = new ChamadaTo(); 
+			ClasseTo classe = new ClasseTo();
+			DisciplinaTo disciplina = new DisciplinaTo();
+			disciplina.setNome(chamadaBean.getDisciplina().getNome());
+			classe.setSerie(chamadaBean.getClasse().getSerie());
+			classe.setTurma(chamadaBean.getClasse().getTurma());
+			classe.setDescricaoSala(chamadaBean.getClasse().getDescricaoSala());
+			chamadaTo.setClasse(classe);
+			chamadaTo.setDisciplina(disciplina);
+			listaChamadasTo.add(chamadaTo);
+		}
+		return listaChamadasTo;
 	}
 
 }
