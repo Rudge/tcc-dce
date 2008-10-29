0a5b189e010e2cfb96fd4b22ed0b616cd0dc2869
rudgee <rudgee@5ebc2e22-5e54-0410-bb67-db0534461473>
2008-10-29 21:46:03 +0000

    Criação dos tratamentos de erro e da consulta de chamadas por data.

--- trunk/dce/WebContent/webpages/erro.jsp
+++ trunk/dce/WebContent/webpages/erro.jsp
@@ -1,6 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"  pageEncoding="ISO-8859-1" isErrorPage="true" %>  
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+<%
+	String erro = (String) request.getAttribute("mensagemErro");
+	if (erro == null || erro.equals("")) {
+    	erro = "Erro ao realizar operação. Por favor contacte o suporte.";
+    }
+%>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>DCE</title>
@@ -11,7 +17,11 @@
         <div id="topo">	
         </div>
         <div id="principal">
-        	<a href="javascript:history.go(-1)">Voltar</a>
+			<div id="frmLogin">
+				<label class="labelErro"><%=erro%></label>
+				<br>
+        		<a href="javascript:history.go(-1)">Voltar</a>
+			</div>
         </div>
         <div id="rodape"/>	
     </div>
--- trunk/dce/src/com/unisa/tcc/dao/ControladorChamadasDAO.java
+++ trunk/dce/src/com/unisa/tcc/dao/ControladorChamadasDAO.java
@@ -3,8 +3,8 @@ package com.unisa.tcc.dao;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 import com.unisa.tcc.bean.ChamadaBean;
@@ -14,23 +14,24 @@ import com.unisa.tcc.propriedades.DceException;
 
 public class ControladorChamadasDAO extends TransactionManager{
 
-	public List<ChamadaBean> consultarChamadasPorData(int idProfessor, GregorianCalendar data) throws DceException{
+	public List<ChamadaBean> consultarChamadasPorData(int idProfessor, int ano, int mes, int dia) throws DceException, SQLException{
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
 		List<ChamadaBean> listaChamadas= new ArrayList<ChamadaBean>();
 		ChamadaBean chamadaBean = new ChamadaBean();
 		StringBuffer query = new StringBuffer();
 		try{
 			conn = getConn();
-			query.append("SELECT DISC.NOME AS DISCIPLINA, CL.SERIE_SERIE, CL.SERIE_TURMA, CL.DESCRICAO_SALA " +
+			query.append("SELECT DISTINCT DISC.NOME AS DISCIPLINA, CL.SERIE_SERIE, CL.SERIE_TURMA, CL.DESCRICAO_SALA " +
 						 "FROM CHAMADA CH, CLASSE_CHAMADA CLCH, CLASSE CL, DISCIPLINAS DISC " +
 						 "WHERE CH.DATA_CHAMADA = ? " + 
 						 "AND DISC.PROFESSOR_IDPROFESSOR = ? " +
 						 "AND CH.DISCIPLINAS_IDDISCIPLINA = DISC.IDDISCIPLINA " +
-						 "AND CL.IDCLASSE = CLCH.CLASSE_IDCLASSE " +
-						 "AND DISC.IDDISCIPLINA = CH.DISCIPLINAS_IDDISCIPLINA ");
-			PreparedStatement stmt = conn.prepareStatement(query.toString());
-			stmt.setDate(1, new Date(data.getGregorianChange().getTime()));
+						 "AND CL.IDCLASSE = CLCH.CLASSE_IDCLASSE ");
+			stmt = conn.prepareStatement(query.toString());
+			stmt.setDate(1, new Date( new java.util.GregorianCalendar(ano, mes, dia).getTimeInMillis()));
 			stmt.setInt(2, idProfessor);
-			ResultSet rs = stmt.executeQuery();
+			rs = stmt.executeQuery();
 			while(rs.next()){
 				DisciplinaBean disciplina = new DisciplinaBean();
 				ClasseBean classe = new ClasseBean();
@@ -42,11 +43,12 @@ public class ControladorChamadasDAO extends TransactionManager{
 				chamadaBean.setDisciplina(disciplina);
 				listaChamadas.add(chamadaBean);
 			}
+		}catch (SQLException e) {
+			throw new DceException("Erro na consulta das chamadas por data!");
+		}finally{
 			rs.close();
 			stmt.close();
 			conn.close();
-		}catch (Exception e) {
-			throw new DceException("Erro na consulta das chamadas por data!");
 		}
 		return listaChamadas;
 	}
